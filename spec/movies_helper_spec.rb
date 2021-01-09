dcce4027443e798416ff67327fb3acfd24a0ce31
Naveen <navi.mehta@gmail.com>
2021-01-09 22:12:59 +1100

    added rspec tests for filmworld API

--- spec/movies_helper_spec.rb
+++ spec/movies_helper_spec.rb
@@ -29,3 +29,32 @@ describe 'Cinema World API Tests' do
         end
     end
 end 
+
+describe 'Film World API Tests' do
+    context '#fetch_movies' do
+        movies = fetch_movies('filmworld')
+        describe 'HTTParty response from Filmworld API' do 
+            it 'should return true' do        
+                expect(movies.success?).to eq true 
+            end
+        end
+    end
+
+    context '#fetch_movie_details' do
+        movie_details = fetch_movie_details('fw2488496')
+        describe 'Search movie by ID' do 
+            it 'should return true' do        
+                expect(movie_details["ID"] == "fw2488496").to eq true 
+            end
+        end
+    end
+
+    context '#fetch_price' do
+        movie_price = fetch_price('filmworld', 'fw0076759')
+        describe 'Filmworld price for Movie ID:- fw0076759' do 
+            it 'should return 22.9' do        
+                expect(movie_price).to eq 22.9 
+            end
+        end
+    end
+end 
